7409517bee2305ad83a21746797f00f3d600a912
ramintheredmn <redcrossprog@gmail.com>
2023-12-28 17:19:51 +0330

    format

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -3,27 +3,27 @@ return {
     "williamboman/mason.nvim",
     config = function()
       require("mason").setup()
-    end
+    end,
   },
   {
     "williamboman/mason-lspconfig.nvim",
     config = function()
       require("mason-lspconfig").setup({
-        ensure_installed = {"lua_ls", "tsserver", "jedi_language_server", "rust_analyzer", "pyright"}
+        ensure_installed = { "lua_ls", "tsserver", "jedi_language_server", "rust_analyzer", "pyright" },
       })
-    end
+    end,
   },
   {
-    'neovim/nvim-lspconfig',
+    "neovim/nvim-lspconfig",
     config = function()
       local lspconfig = require("lspconfig")
 
       lspconfig.lua_ls.setup({})
       lspconfig.tsserver.setup({})
       lspconfig.pyright.setup({})
-      vim.keymap.set('n', '<leader>da', vim.lsp.buf.definition, opts)
-      vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, opts)
-      vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
-    end
+      vim.keymap.set("n", "<leader>da", vim.lsp.buf.definition, opts)
+      vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+    end,
   },
 }
