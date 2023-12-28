d675149d42057f0ec618d7be03af02de29e6f4f4
ramintheredmn <redcrossprog@gmail.com>
2023-12-28 20:35:01 +0330

    auto complete

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -18,12 +18,56 @@ return {
     config = function()
       local lspconfig = require("lspconfig")
 
-      lspconfig.lua_ls.setup({})
-      lspconfig.tsserver.setup({})
-      lspconfig.pyright.setup({})
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+      lspconfig.lua_ls.setup({
+
+        capabilities = capabilities,
+      })
+      lspconfig.tsserver.setup({
+        capabilities = capabilities,
+      })
+      lspconfig.pyright.setup({
+        capabilities = capabilities,
+      })
       vim.keymap.set("n", "<leader>da", vim.lsp.buf.definition, opts)
       vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
       vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+    end,
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    config = function()
+      local cmp = require("cmp")
+      cmp.setup({
+        mapping = {
+          ["<Tab>"] = function(fallback)
+            if cmp.visible() then
+              cmp.select_next_item()
+            else
+              fallback()
+            end
+          end,
+          ["<S-Tab>"] = function(fallback)
+            if cmp.visible() then
+              cmp.select_prev_item()
+            else
+              fallback()
+            end
+          end,
+        },
+      })
+    end,
+  },
+  {
+
+    "hrsh7th/cmp-nvim-lsp",
+
+    config = function()
+      require("cmp").setup({
+        sources = {
+          { name = "nvim_lsp" },
+        },
+      })
     end,
   },
 }
