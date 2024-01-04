c40d464e25b7f22e658563266dbb477bca4c6a1c
ramintheredmn <redcrossprog@gmail.com>
2024-01-04 10:13:32 +0330

    htmx language support added and some minor chnages

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,22 +1,42 @@
 return {
-	{
+  {
 
-		"nvimtools/none-ls.nvim",
-		config = function()
-			local null_ls = require("null-ls")
+    "nvimtools/none-ls.nvim",
+    config = function()
+      local null_ls = require("null-ls")
 
-			null_ls.setup({
-        ensure_istalled = {"stylua", "black", "prettier", "eslint_d", "codespell", "ruff"},
-				sources = {
-					null_ls.builtins.formatting.stylua,
-					null_ls.builtins.formatting.black,
-					null_ls.builtins.formatting.prettier,
-					null_ls.builtins.diagnostics.eslint_d,
-					null_ls.builtins.diagnostics.codespell,
-					null_ls.builtins.diagnostics.ruff,
-				},
-			})
-			vim.keymap.set("n", "<leader>fm", vim.lsp.buf.format, {})
-		end,
-	},
+      null_ls.setup({
+        ensure_istalled = {},
+        sources = {
+          null_ls.builtins.formatting.stylua,
+          null_ls.builtins.formatting.black,
+          null_ls.builtins.formatting.prettier,
+          null_ls.builtins.diagnostics.eslint_d,
+          null_ls.builtins.diagnostics.codespell,
+          null_ls.builtins.diagnostics.ruff,
+        },
+      })
+      vim.keymap.set("n", "<leader>fm", vim.lsp.buf.format, {})
+    end,
+  },
+  {
+      "jay-babu/mason-null-ls.nvim",
+      event = { "BufReadPre", "BufNewFile" },
+      dependencies = {
+        "williamboman/mason.nvim",
+        "jose-elias-alvarez/null-ls.nvim",
+      },
+      config = function()
+        require("mason-null-ls").setup({
+          ensure_installed = {
+            "stylua",
+            "black",
+            "prettier",
+            "eslint_d",
+            "codespell",
+            "ruff",
+          },
+        })
+      end,
+  },
 }
